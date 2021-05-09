d75ca89ecd7fde84ff1ad93fa07f1de423a43bf2
rgu <rgu@novatec-gmbh.de>
2021-05-09 14:27:13 +0200

    setup06#patch: added actuator w/ health endpoint

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
