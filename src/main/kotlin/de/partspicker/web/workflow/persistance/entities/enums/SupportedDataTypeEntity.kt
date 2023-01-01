81f3efc0c8f420a76fd8e9c33817fde4d05c4cef
RobinGuth <robin.guth@gmx.de>
2023-01-01 22:10:40 +0100

    PICK-96: Add service to set instance values

--- src/main/kotlin/de/partspicker/web/workflow/persistance/entities/enums/SupportedDataTypeEntity.kt
+++ src/main/kotlin/de/partspicker/web/workflow/persistance/entities/enums/SupportedDataTypeEntity.kt
@@ -1,6 +1,15 @@
 package de.partspicker.web.workflow.persistance.entities.enums
 
+import de.partspicker.web.workflow.business.objects.enums.SupportedDataType
+
 enum class SupportedDataTypeEntity {
     STRING,
-    LONG
+    LONG;
+
+    companion object {
+        fun from(supportedDataType: SupportedDataType) = when (supportedDataType) {
+            SupportedDataType.STRING -> STRING
+            SupportedDataType.LONG -> LONG
+        }
+    }
 }
